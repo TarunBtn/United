45dd0f6ade4ece4b588924f513dcf55dcf5587fa
no02 <no02@192.168.1.206>
2023-09-14 12:58:07 +0530

    Update script-HomePageAdmin

--- UnitedOne/src/main/java/com/united/qa/pages/HomePageAdmin.java
+++ UnitedOne/src/main/java/com/united/qa/pages/HomePageAdmin.java
@@ -60,6 +60,9 @@ public class HomePageAdmin extends TestBase{
 			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[2]/div[1]/ul/li[4]/div/div/div/div[2]/ul/span[5]/li/a")
 			WebElement userManagementTab;
 			
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[2]/div[1]/ul/li[4]/div/div/div/div[2]/ul/span[6]/li/a/font/font")
+			WebElement allCompanies;
+			
 				
 			//Initialization
 			public HomePageAdmin() {
@@ -140,6 +143,10 @@ public class HomePageAdmin extends TestBase{
 			
 			public void clickUserManagementTab() {
 				userManagementTab.click();
+			}
+			
+			public void clickAllCompaniesTab() {
+				allCompanies.click();			
 			}
 
 }
